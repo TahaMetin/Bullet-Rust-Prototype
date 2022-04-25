af812fb902d7e1e4162978289e97ae39d5c475a9
Taha <55713590+TahaMetin@users.noreply.github.com>
2022-04-25 06:14:08 +0300

    Remaining Enemy Count ADEED
    Remaining enemy count added to UI

--- Bullet Rush Prototype/Assets/Scripts/ProgressBar.cs
+++ Bullet Rush Prototype/Assets/Scripts/ProgressBar.cs
@@ -9,9 +9,12 @@ public class ProgressBar : Singleton<ProgressBar>
     public Image mask;
     public GameObject enemys;
     private bool isGameRunning;
+    [SerializeField] GameObject numberOfRemainingEnemy_GO;
+    Text numberOfRemainingEnemy_Text;
     private void Start()
     {
         SetMaxEnemyCount();
+        numberOfRemainingEnemy_Text = numberOfRemainingEnemy_GO.GetComponent<Text>();
     }
 
     private void Update()
@@ -26,7 +29,7 @@ public class ProgressBar : Singleton<ProgressBar>
                 activeEnemyCount++;
         }
         current = activeEnemyCount; // set current active enemy number
-
+        numberOfRemainingEnemy_Text.text = current.ToString();
         SetCurrentFill();
     }
     public void SetCurrentFill() 
@@ -36,6 +39,7 @@ public class ProgressBar : Singleton<ProgressBar>
         
         if(current  == 0)  // if there is no enemy alive end the game
         {
+            numberOfRemainingEnemy_Text.text = current.ToString();
             mask.fillAmount = 0f;
             EventManager.Instance.win.Invoke();
             isGameRunning = false;
