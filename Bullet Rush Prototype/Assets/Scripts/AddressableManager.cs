484b79a6146d7c34ffa7e246a5375a3ec2493c03
Taha <55713590+TahaMetin@users.noreply.github.com>
2022-06-07 17:10:44 +0300

    Asset changes

---
+++ Bullet Rush Prototype/Assets/Scripts/AddressableManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.AddressableAssets.ResourceLocators;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class AddressableManager : MonoBehaviour
+{
+    [SerializeField]
+    AssetReference bigEnemyAssetReference;
+   
+
+}
